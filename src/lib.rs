4e465a47b11689b95519b4150790bc39e07a752e
George Burton <burtonageo@gmail.com>
2015-07-18 11:35:59 +0100

    Remove #![feature(core)] and replace it with feature(cmp_partial)

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 // AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#![feature(core)]
+#![feature(cmp_partial)]
 #![deny(warnings)]
 #![warn(missing_docs)]
 
